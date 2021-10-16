5d343c1f8dc2902f6159f8b2e31f9b21b8261ee5
ShannaJSmith <shanna_smith987@hotmail.com>
2021-10-16 16:59:57 +0000

    created tables for database

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,35 @@
 -- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS stories CASCADE;
+DROP TABLE IF EXISTS contributions CASCADE;
+DROP TABLE IF EXISTS votes CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  avatar_url VARCHAR (255),
+  is_author BOOLEAN NOT NULL
+);
+
+CREATE TABLE stories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  status BOOLEAN NOT NULL,
+  thumbnail_url VARCHAR (255) NOT NULL,
+  author_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE contributions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  accepted BOOLEAN NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE votes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  vote bit,
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
+  author_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
