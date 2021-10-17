c7cdc1269b71220051b4d3f300cec88418f734cc
Maram Qdah <maram.m.qdah@gmail.com>
2021-10-17 14:34:21 -0400

    delete errors

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,13 +3,3 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 5e72993dd98b4a8355ad4abaee3e36e1308ef601
->>>>>>> 0a174fe40175f08df7469795f9a1b661947c36a9
-=======
->>>>>>> fa6286557855f5ddc6bd4bfe0e0380d5ca264645
