4f01922973300ffbc0e3065d603b1a81be8f3397
ShannaJSmith <shanna_smith987@hotmail.com>
2021-10-17 16:05:25 +0000

    removed avatar_url from database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, avatar_url, is_author)
-VALUES ('Alice', 'http:', TRUE),
-('Kira', 'http:', FALSE),
-('Daniel', 'http:', FALSE);
+INSERT INTO users (name, is_author)
+VALUES ('Alice', TRUE),
+('Kira', FALSE),
+('Daniel', FALSE);
 
 INSERT INTO stories (title, content, status, thumbnail_url)
 VALUES ('The Little Mermaid', 'LOTS OF TEXT', TRUE, 'HTTP:'),
@@ -19,7 +19,4 @@ VALUES (2, 1, 1, TRUE),
 (2, 1, 1, FALSE),
 (2, 1, 1, FALSE);
 
-SELECT users.name, contributions.suggestion, count(votes.vote) as vote_count
-FROM users
-JOIN contributions ON user_id = users.id
-JOIN votes ON contribution_id = contributions.id;
+
--- db/seeds/Useful_Selects.sql
+++ db/seeds/Useful_Selects.sql
@@ -8,4 +8,11 @@ JOIN votes ON contribution_id = contributions.id
 WHERE votes.vote = TRUE
 GROUP BY users.name, contributions.suggestion;
 
+RETURNS THE AMOUNT OF UPVOTES (TRUE) FOR A SPECIFIC CONTRIBUTION:
+
+SELECT COUNT(votes.vote) as vote_count
+FROM votes
+JOIN contributions ON contributions.id = contribution_id
+WHERE contributions.id = 1 AND votes.vote = TRUE;
+
 /*
