5e72993dd98b4a8355ad4abaee3e36e1308ef601
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-17 18:11:27 +0000

    Created index, nav, main, layout css files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-=======
-// Client facing scripts here
-// hi
->>>>>>> d82a385838b747d1c0c4b872efd319a3c93a71e5
